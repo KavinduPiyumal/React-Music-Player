37a3a93c6e5d013df1b25c9bc23f2c583dac1e74
Kavindu Piyumal <42784631+KavinduPiyumal@users.noreply.github.com>
2022-01-16 13:42:06 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,88 @@
+import { v4 as uuidv4 } from "uuid";
+function chillHop() {
+  return [
+    {
+      name: "Rap Malsara",
+      cover:
+        "https://pricein.lk/wp-content/uploads/2021/12/Rap-Malsara-Dulan-ARX-300x300.jpg",
+      artist: "Dulan ARX",
+      audio: "https://sinhanada.net/data/file/20301/Rap-Malsara---Dulan-ARX-mp3.html?download",
+      color: ["#530406", "#DA7A48"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Nasuna",
+      cover:
+        "https://www.mp3song.lk/images/song-img/400/16385_thumb.jpg",
+      artist: "Smokio Ft. Dinesh Gamage",
+      audio: "https://sinhanada.net/data/file/16164/Nasuna---Smokio-Ft--Dinesh-Gamage-mp3.html?download",
+      color: ["#1F2920", "#528401"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Lelena",
+      cover:
+        "https://m.sinhanada.net/icon.php?src=Li9maWxlcy9OZXcgU2luaGFsYSBNcDMvTGVsZW5hIC0gTmlsYW4gSGV0dGlhcmFjaGFoaS5tcDM=",
+      artist: "Nilan Hettiarachahi",
+      audio: "https://m.sinhanada.net/data/file/16108/Lelena---Nilan-Hettiarachahi-mp3.html?download",
+      color: ["#C68739", "#140603"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Wahi Bindu Watenawanam",
+      cover:
+        "https://song.sgp1.cdn.digitaloceanspaces.com/image/song/2021/07/13164.jpg",
+      artist: "Dinesh Gamage",
+      audio: "https://sinhanada.net/data/file/16709/Wahi-Bindu-Watenawanam---Dinesh-Gamage-mp3.html?download",
+      color: ["#79594C", "#E6B2A7"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Adariye",
+      cover:
+        "https://www.sangeethe.com/assets/images/300/1612101194_adariye.jpg",
+      artist: "Dhanith Sri",
+      audio: "https://sinhanada.net/data/file/12269/Adariye---Dhanith-Sri-mp3.html?download",
+      color: ["#2C4D4B", "#5D9E96"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Danena Thuru Maa",
+      cover:
+        "https://tune.lk/storage/app/public/img/artist/1582974342.jpg",
+      artist: "Dinesh Gamage ft Kanchana Anuradhi",
+      audio: "https://sinhanada.net/data/file/7469/Danena-Thuru-Maa---Dinesh-Gamage-ft-Kanchana-Anuradhi-mp3.html?download",
+      color: ["#2B3824", "#57492E"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Vise Vise",
+      cover:
+        "https://sarigama.lk/img/songs/appsong-vnz2z9airygug59l.png",
+      artist: "Centigradz",
+      audio: "https://sinhanada.net/data/file/19093/Vise-Vise---Centigradz-mp3.html?download",
+      color: ["#CD607D", "#c94043"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Rura",
+      cover:
+        "https://www.sangeethe.com/assets/images/300/1635439837_rura.jpg",
+      artist: "Sanju Himala",
+      audio: "https://sinhanada.net/data/file/19866/Rura---Sanjula-Himala-mp3.html?download",
+      color: ["#4C2C1D", "#C0A47C"],
+      id: uuidv4(),
+      active: false,
+    },
+    //ADD MORE HERE
+  ];
+}
+
+export default chillHop;
